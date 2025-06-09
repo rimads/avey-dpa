bc1cd61c92ded60b703c1ee0994ea67193186f93
Rain <dacharya@avey.ai>
2025-06-09 05:05:07 -0400

    Update make_hf_tokenizer.py
    docstring quotes not needed for template

--- make_hf_tokenizer.py
+++ make_hf_tokenizer.py
@@ -14,7 +14,7 @@ tokenizer = GPT2TokenizerFast.from_pretrained(save_dir)
 shutil.rmtree(save_dir)
 
 tokenizer.save_pretrained(f"{save_dir}_base")
-tokenizer.chat_template = """{% for message in messages %}{% if message['role'] == 'user' %}{{ '<|user|>' + message['content'] }}{% elif message['role'] == 'system' %}{{ '<|system|>' + message['content'] }}{% elif message['role'] == 'assistant' %}{{ '<|assistant|>'  + message['content'] }}{% endif %}{% if loop.last and add_generation_prompt %}{{ '<|assistant|>' }}{% endif %}{% endfor %}
-"""
+tokenizer.chat_template = "{% for message in messages %}{% if message['role'] == 'user' %}{{ '<|user|>' + message['content'] }}{% elif message['role'] == 'system' %}{{ '<|system|>' + message['content'] }}{% elif message['role'] == 'assistant' %}{{ '<|assistant|>'  + message['content'] }}{% endif %}{% if loop.last and add_generation_prompt %}{{ '<|assistant|>' }}{% endif %}{% endfor %}
+"
 tokenizer.additional_special_tokens = ["<|system|>", "<|user|>", "<|assistant|>"]
 tokenizer.save_pretrained(f"{save_dir}_it")
